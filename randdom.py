0fbff82676e87afcb9c6ff6707a2fc587ed456ec
Alan Kao <alanods3on@gmail.com>
2020-09-24 15:51:39 +0800

    sec commit

--- randdom.py
+++ randdom.py
@@ -1,14 +1,17 @@
 #使用import載入，並使用random產生隨機變數
 import random
 r = random.randint(1, 100)
-
+count = 0
 while True:
+	count += 1 # = count + 1
 	num = input('請猜一個數字: ')
 	num = int(num)
 	if num == r:
 		print('終於猜對了')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('你猜錯了，你猜得比答案大')
 	else:
-		print('你猜錯了，你猜得比答案小')	
+		print('你猜錯了，你猜得比答案小')
+	print('這是你猜的第', count, '次')	
